4e3080807d98b4144682ea22b533038fb1b68427
irene29-hub <muemarenee@gmail.com>
2025-06-18 22:13:53 +0300

    add changes

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record) {
+  // Use the find() method to iterate through the array of game records.
+  // The callback function checks if the 'result' property of an object is 'W' (for a win).
+  const winRecord = record.find(game => game.result === "W");
+
+  // If a win record is found (i.e., winRecord is not undefined),
+  // return the 'year' property of that record.
+  if (winRecord) {
+    return winRecord.year;
+  } else {
+    // If no win record is found, return undefined as specified.
+    return undefined;
+  }
+}
